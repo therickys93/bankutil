dd8e912c4e2f5aa9933a202249a33a9e887ad24c
Riccardo Crippa <therickys93@gmail.com>
2017-03-07 17:38:39 +0100

    aggiunta classe per i test delle carte di credito

---
+++ src/test/java/it/therickys93/bankutil/CreditCardTest.java
@@ -0,0 +1,29 @@
+package it.therickys93.bankutil;
+
+public class CreditCardTest {
+
+	/*
+	@Test
+	public void testOne() {
+		CreditCard card = CreditCard.createCard("5555555555554444");
+		assertEquals("5555555555554444", card.number());
+	}
+	
+	/*
+	@Test
+	public void testTwo() {
+		CreditCard card = CreditCard.createCard("");
+		assertTrue(card.ok());
+	}
+	
+	/*
+	@Test
+	public void testThree() {
+		CreditCard card = CreditCard.createCard("");
+		assertEquals("Mastercard", card.type());
+	}
+	*/
+	
+	// bisogna testare anche American Express e Visa poi prendere dal sito elenco e vedere se funziona
+	
+}
