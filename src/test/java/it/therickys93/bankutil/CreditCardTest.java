1e2ae60ab11bfbbce87c848ef012ff65676f1b10
Riccardo Crippa <therickys93@gmail.com>
2017-03-08 10:42:42 +0100

    pronto per implementare classe credit card

--- src/test/java/it/therickys93/bankutil/CreditCardTest.java
+++ src/test/java/it/therickys93/bankutil/CreditCardTest.java
@@ -12,18 +12,68 @@ public class CreditCardTest {
 	/*
 	@Test
 	public void testTwo() {
-		CreditCard card = CreditCard.createCard("");
+		CreditCard card = CreditCard.createCard("5555555555554444");
 		assertTrue(card.ok());
 	}
 	
 	/*
 	@Test
 	public void testThree() {
+		CreditCard card = CreditCard.createCard("5555555555554444");
+		assertEquals("MasterCard", card.type());
+	}
+
+	/*
+	@Test
+	public void testFour() {
+		CreditCard card = CreditCard.createCard("4111111111111111");
+		assertEquals("Visa", card.type());
+	}
+	
+	/*
+	@Test
+	public void testFive() {
+		CreditCard card = CreditCard.createCard("378282246310005");
+		assertEquals("American Express", card.type());
+	}
+	
+	/*
+	@Test
+	public void testSix() {
 		CreditCard card = CreditCard.createCard("");
-		assertEquals("Mastercard", card.type());
+		assertFalse(card.ok());
+		assertEquals("Invalid", card.type());
+	}
+	
+	/*
+	@Test
+	public void testSeven() {
+		CreditCard card = CreditCard.createCard(null);
+		assertFalse(card.ok());
+		assertEquals("Invalid", card.type());
+	}
+	
+	/*
+	@Test
+	public void testFinal() {
+		Map<String, CreditCard> cards = getCards();
+		for (Map.Entry<String, CreditCard> entry : cards.entrySet())
+		{
+		    assertEquals(entry.getKey(), entry.getValue().type());
+		}
+	}
+	
+	private Map<String, CreditCard> getCards() {
+		Map<String, CreditCard> cards = new HashMap<String, CreditCard>();
+		cards.add("American Express", CreditCard.createCard("378282246310005"));
+		cards.add("American Express", CreditCard.createCard("371449635398431"));
+		cards.add("American Express", CreditCard.createCard("378734493671000"));
+		cards.add("MasterCard", CreditCard.createCard("5555555555554444"));
+		cards.add("MasterCard", CreditCard.createCard("5105105105105100"));
+		cards.add("Visa", CreditCard.createCard("4111111111111111"));
+		cards.add("Visa", CreditCard.createCard("4012888888881881"));
+		cards.add("Visa", CreditCard.createCard("4222222222222"));
+		return cards;
 	}
 	*/
-	
-	// bisogna testare anche American Express e Visa poi prendere dal sito elenco e vedere se funziona
-	
 }
